1da396c50f68c35f81f6692b87f089b94cb47c48
Amparo <60093666+aescortell@users.noreply.github.com>
2025-09-07 17:05:35 +0200

    Create apps.sh

---
+++ scripts/apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apps=(iterm2 slack microsoft-teams google-chrome firefox 1password webstorm visual-studio-code docker)
+for app in "${apps[@]}"; do
+    if brew list --cask "$app" &>/dev/null; then
+        echo "✅ $app ya está instalado"
+    else
+        echo "⬇️ Instalando $app..."
+        brew install --cask "$app"
+    fi
+done
