e3604aaf9a117ce697d15bbd8394c93485ad9095
Amparo <60093666+aescortell@users.noreply.github.com>
2025-09-08 13:13:59 +0200

    Update apps.sh

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-apps=(iterm2 slack microsoft-teams google-chrome firefox 1password webstorm visual-studio-code docker)
+apps=(
+    iterm2
+    slack
+    microsoft-teams
+    google-chrome
+    firefox
+    1password
+    webstorm
+    visual-studio-code
+    docker
+    notion
+    mongodb-compass
+    dbeaver-community
+)
+
 for app in "${apps[@]}"; do
     if brew list --cask "$app" &>/dev/null; then
         echo "✅ $app ya está instalado"
